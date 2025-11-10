617705c6e3b7567b47d4ba4d17c23476ca12f905
jump-and-jump <984292420@qq.com>
2025-11-11 00:51:50 +0800

    feat： change code

--- src/routes/modal.rs
+++ src/routes/modal.rs
@@ -2,7 +2,7 @@ use askama::Template;
 use askama_axum::IntoResponse;
 
 #[derive(Template)]
-#[template(path = "components/modal/example.html")]
+#[template(path = "components/modal/base.html")]
 pub struct ModalExampleTemplate;
 
 pub async fn example() -> impl IntoResponse {
